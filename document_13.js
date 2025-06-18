f5a9b1af11075174072c64a0580d549292c9468b
Darshit-coder <darshittejani8233@gmail.com>
2025-06-18 15:33:12 +0530

    Update document_13.js

--- document_13.js
+++ document_13.js
@@ -7,7 +7,7 @@
     form.addEventListener('submit', function(e) {
       e.preventDefault();
       if (input.value === ",''Z@s!!O[=CbbW") {
-        window.location.href = "https://grannyandsmith-my.sharepoint.com/personal/lb_grannyandsmith_com/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Flb%5Fgrannyandsmith%5Fcom%2FDocuments%2FProjekt%5FDubai&ga=1";
+        window.location.href = "https://www.youtube.com/";
       } else {
         alert("Incorrect password.");
         input.value = '';
