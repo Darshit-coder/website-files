d6046c440168564f0ed971b0f3d1d07f97dcd8b7
Darshit-coder <darshittejani8233@gmail.com>
2025-06-18 23:41:39 +0530

    Update document_mallorca.js

--- document_mallorca.js
+++ document_mallorca.js
@@ -8,7 +8,7 @@
       e.preventDefault();
 
       
-      if (input.value === "123456789") {
+      if (input.value === "N93PA2JO") {
         window.location.href = "https://grannyandsmith-my.sharepoint.com/personal/lb_grannyandsmith_com/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Flb%5Fgrannyandsmith%5Fcom%2FDocuments%2FProjekt%5FMallorca&ga=1";
       } else {
         alert("Incorrect password.");
