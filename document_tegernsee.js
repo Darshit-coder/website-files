43b49472dcac555a287c5dc77c9cb2c158df82a1
Darshit-coder <darshittejani8233@gmail.com>
2025-06-18 16:15:26 +0530

    Add files via upload

---
+++ document_tegernsee.js
@@ -0,0 +1,25 @@
+(function () {
+  function init() {
+    const form = document.getElementById('passwordFormtegernsee');
+    const input = document.getElementById('passwordInputtegernsee');
+    if (!form || !input) return;
+
+    form.addEventListener('submit', function(e) {
+      e.preventDefault();
+
+      
+      if (input.value === "123456789") {
+        window.location.href = "https://grannyandsmith-my.sharepoint.com/personal/lb_grannyandsmith_com/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Flb%5Fgrannyandsmith%5Fcom%2FDocuments%2FProjekt%5FTegernsee&ga=1";
+      } else {
+        alert("Incorrect password.");
+        input.value = '';
+      }
+    });
+  }
+
+  if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', init);
+  } else {
+    init();
+  }
+})();
