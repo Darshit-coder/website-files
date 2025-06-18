07bc7aac1c25b3c66127fad4774e72081c00621e
Darshit-coder <darshittejani8233@gmail.com>
2025-06-18 23:40:57 +0530

    Update document_12.js

--- document_12.js
+++ document_12.js
@@ -6,7 +6,7 @@
 
     form.addEventListener('submit', function(e) {
       e.preventDefault();
-      if (input.value === "123456789") {
+      if (input.value === "DSCV6X63") {
         window.location.href = "https://grannyandsmith-my.sharepoint.com/personal/lb_grannyandsmith_com/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Flb%5Fgrannyandsmith%5Fcom%2FDocuments%2FProjekt%5FDubai&ga=1";
       } else {
         alert("Incorrect password.");
