6685546be9c031c1b90e123e996dc14029cfc1b5
Darshit-coder <darshittejani8233@gmail.com>
2025-06-18 18:24:05 +0530

    Update document_tegernsee.js

--- document_tegernsee.js
+++ document_tegernsee.js
@@ -1,3 +1,4 @@
+
 (function () {
   function init() {
     const form = document.getElementById('passwordFormtegernsee');
@@ -6,8 +7,6 @@
 
     form.addEventListener('submit', function(e) {
       e.preventDefault();
-
-      
       if (input.value === "123456789") {
         window.location.href = "https://grannyandsmith-my.sharepoint.com/personal/lb_grannyandsmith_com/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Flb%5Fgrannyandsmith%5Fcom%2FDocuments%2FProjekt%5FTegernsee&ga=1";
       } else {
